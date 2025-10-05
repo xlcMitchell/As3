30e898df47ea89b41142def798fd3220d693dc7c
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-10-05 15:41:32 +1300

    Compile success

--- app/src/main/java/com/example/bit603_mitchell_travis_5080526_as3/MainActivity.java
+++ app/src/main/java/com/example/bit603_mitchell_travis_5080526_as3/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
