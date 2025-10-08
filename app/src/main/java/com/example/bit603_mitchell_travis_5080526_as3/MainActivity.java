d2d25233ef414601b88ff9f642c7766206758a84
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-10-08 16:04:04 +1300

    youtube player activty added

--- app/src/main/java/com/example/bit603_mitchell_travis_5080526_as3/MainActivity.java
+++ app/src/main/java/com/example/bit603_mitchell_travis_5080526_as3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bit603_mitchell_travis_5080526_as3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         ChannelViewModel channelInsert = new ChannelViewModel();
         channelInsert.writeToFirebase();
         channelInsert.readChannel("oNATRW73xJS9frychNr7");
+
+        Button playerBtn = findViewById(R.id.watchVideo);
+        playerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,YouTubePlayerActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
